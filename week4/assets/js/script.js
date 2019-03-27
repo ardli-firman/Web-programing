3014b9eefec93a25ff19e3c44fe890fcf568d371
a0i <estungtung59@gmail.com>
2019-03-27 11:13:09 +0700

    Pertemuan5 (Company Website)

---
+++ week4/assets/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.header').height($(window).height());
+    $('.navbar a').click(function () {
+        $('body,html').animate({
+            scrollTop: $('#' + $(this).data('value')).offset().top
+        }, 1000)
+    })
+})
